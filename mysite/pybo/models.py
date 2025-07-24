91f2e4debe8b95a933096e29d718ad508574163c
ataraxia7899 <ataraxia7899@gmail.com>
2025-07-24 16:00:16 +0900

    오늘 수정한 내용 초기화

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'[{self.id}] {self.content[:20]} by {self.author.username}'
